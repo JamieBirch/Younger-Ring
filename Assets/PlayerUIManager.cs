77f213b84a1e14ca79113035830bdf07f2319827
JamieBirch <jamie.d.birch@gmail.com>
2023-05-12 13:02:24 +0200

    Initial commit

---
+++ Assets/PlayerUIManager.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    public static PlayerUIManager instance;
+    [Header("NETWORK JOIN")] [SerializeField]
+    private bool startGameAsClient;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (startGameAsClient)
+        {
+            startGameAsClient = false;
+            NetworkManager.Singleton.Shutdown();
+
+            NetworkManager.Singleton.StartClient();
+        }
+    }
+}
